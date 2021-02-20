083cd1aa97758ab2a8389f05d51307ff349c5c1d
devindianushika <devindianushika@gmail.com>
2021-02-20 17:04:59 +0530

    Edited get method logic in character controller

--- Controllers/CharacterController.cs
+++ Controllers/CharacterController.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using dotnet_rpg.Models;
 using Microsoft.AspNetCore.Mvc;
 
@@ -5,14 +6,23 @@ namespace dotnet_rpg.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class CharacterController: ControllerBase
+    public class CharacterController : ControllerBase
     {
-        private static Character knight = new Character();
-    
-
-    public IActionResult Get()
+        private static List<Character> characters = new List<Character>(){
+    new Character(),
+    new Character { Name = "Sam",Intelligence=20,Strength=258
+    }
+        };
+        [HttpGet]
+        [Route("Getall")]
+        public IActionResult Get()
         {
-            return Ok(knight);
+            return Ok(characters);
+        }
+        [HttpGet]
+        public IActionResult GetSingleCharacter()
+        {
+            return Ok(characters[0]);
         }
 
     }
