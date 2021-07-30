5f551787a3982aa1f6b6846348d5b058cc7282b8
devindianushika <devindianushika@gmail.com>
2021-07-30 19:52:09 +0530

    Implement character service functionalities

--- Controllers/CharacterController.cs
+++ Controllers/CharacterController.cs
@@ -4,22 +4,28 @@ using dotnet_rpg.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace dotnet_rpg.Controllers
+
 {
     [ApiController]
     [Route("[controller]")]
     public class CharacterController : ControllerBase
-    {
+      {
+   
         private static List<Character> characters = new List<Character>(){
     new Character(),
     new Character {Id= 1, Name = "Sam",Intelligence=20,Strength=258
     }
         };
+
+
         [HttpGet]
         [Route("Getall")]
         public IActionResult Get()
         {
             return Ok(characters);
         }
+
+
         [HttpGet("{id}")]
         public IActionResult GetSingleCharacter(int id)
         {
--- Services/CharacterService/ICharcterService.cs
+++ Services/CharacterService/ICharcterService.cs
@@ -1,7 +1,15 @@
+using System.Collections.Generic;
+using dotnet_rpg.Models;
 namespace dotnet_rpg.Services.CharacterService
 {
     public interface ICharcterService
     {
          
+     public List<Character> GetAllCharacters();
+     public Character GetCharacterById(int id);
+     public List<Character> AddCharacter(Character newCharacter);
+
+     
+
     }
 }
